5c7dc324865fa64c363ad5093b6b4baaae2fec61
root <root@localhost.localdomain>
2015-03-07 18:52:49 +0800

    lab0-spoc-disscussion

---
+++ related_info/lab0/lab0_ex3.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: lab0_ex3.c
+	> Author: 
+	> Mail: 
+    > Created Time: Sat 07 Mar 2015 04:35:43 PM CST
+ ************************************************************************/
+
+#include <stdlib.h>
+#include "../../labcodes/lab2/libs/list.h"
+
+int main()
+{
+    list_entry_t* ll = (list_entry_t*)malloc(sizeof(list_entry_t)); 
+    list_init(ll);
+    return 0;
+}
+
